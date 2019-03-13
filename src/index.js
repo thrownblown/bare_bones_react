cdb7d3d542b614ed5eca788190e7a88a4189694e
Alex Farioletti <gsf.nyc@gmail.com>
2019-03-13 15:47:28 -0700

    loginlookinggood

--- src/index.js
+++ src/index.js
@@ -3,64 +3,88 @@ import ReactDOM from 'react-dom';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 
-const conty = (
-<Container>
-  <Row>
-    <Col>1 of 2</Col>
-    <Col>2 of 2</Col>
-  </Row>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col id="mid">2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
-	);
+const top_nav = (
+	<Navbar bg="dark" variant="dark" fixed="top" >
+	  <Navbar.Brand href="#home">TJ</Navbar.Brand>
+	  <Nav className="mr-auto">
+	    <Nav.Link href="#home">My Jobs</Nav.Link>
+	    <Nav.Link href="#features">All Jobs</Nav.Link>
+	    <Nav.Link href="#pricing">Unassigned</Nav.Link>
+	  </Nav>
+	</Navbar>
+);
 
-const jumb = (
-<Jumbotron>
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <div id="board"></div>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>
-	);
-
-const foo_form = (
-<Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicChecbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+const bottom_nav = (
+  <Navbar bg="light" variant="light" fixed="bottom" >
+    <Navbar.Brand href="#home">TJ</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">My Jobs</Nav.Link>
+      <Nav.Link href="#features">All Jobs</Nav.Link>
+      <Nav.Link href="#pricing">Unassigned</Nav.Link>
+    </Nav>
+  </Navbar>
 );
 
 
+const conty = (
+  <Container className="h-100">
+    <Row className="h-100">
+      <Col id="mid" className="my-auto"></Col>
+    </Row>
+  </Container>
+);
+
+const jumb = (
+  <Jumbotron>
+    <div id="board"></div>
+  </Jumbotron>
+);
+
+const login_form = (
+	<Form>
+	  <Form.Group controlId="formBasicEmail">
+	    <Form.Label>Email address</Form.Label>
+	    <Form.Control type="email" placeholder="Enter email" />
+	    <Form.Text className="text-muted">
+	      We'll never share your email with anyone else.
+	    </Form.Text>
+	  </Form.Group>
+
+	  <Form.Group controlId="formBasicPassword">
+	    <Form.Label>Password</Form.Label>
+	    <Form.Control type="password" placeholder="Password" />
+	  </Form.Group>
+	  <Form.Group controlId="formBasicChecbox">
+	    <Form.Check type="checkbox" label="Check me out" />
+	  </Form.Group>
+	  <Button variant="primary" type="submit" className="pull-rigt">
+	    Submit
+	  </Button>
+	</Form>
+);
+
+
+
+
+
+
+
+ReactDOM.render(
+    top_nav,
+    document.getElementById('top_nav')
+);
+ReactDOM.render(
+    bottom_nav,
+    document.getElementById('bottom_nav')
+);
 ReactDOM.render(
     conty,
     document.getElementById('root')
@@ -72,6 +96,6 @@ ReactDOM.render(
 );
 
 ReactDOM.render(
-    foo_form,
+    login_form,
     document.getElementById('board')
 );
