1659f839c3db43362d70a37acd8dd72d8902569b
Alex Farioletti <gsf.nyc@gmail.com>
2019-03-13 10:15:51 -0700

    ditch material basic bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+
+const conty = (
+<Container>
+  <Row>
+    <Col>1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col id="mid">2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+	);
+
+const jumb = (
+<Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <div id="board"></div>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+	);
+
+const foo_form = (
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicChecbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+);
+
+
 ReactDOM.render(
-    <h1>Bare Bones React Is Working!</h1>,
+    conty,
     document.getElementById('root')
-)
+);
+
+ReactDOM.render(
+    jumb,
+    document.getElementById('mid')
+);
+
+ReactDOM.render(
+    foo_form,
+    document.getElementById('board')
+);
